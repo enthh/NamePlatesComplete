cfd501296de0248b0510a1cae8ca2af638eaeae0
Enth <wowenth@gmail.com>
2023-06-10 12:56:17 +0200

    OnEvent is better than hooks

--- NamePlatesComplete_AuraTooltip/NamePlatesComplete_AuraTooltip.lua
+++ NamePlatesComplete_AuraTooltip/NamePlatesComplete_AuraTooltip.lua
@@ -1,16 +1,30 @@
 NamePlatesComplete_AuraTooltipDriverMixin = {}
 
 function NamePlatesComplete_AuraTooltipDriverMixin:OnLoad()
-    hooksecurefunc(NamePlateDriverFrame, "OnNamePlateCreated", function(_, nameplateBase)
-        hooksecurefunc(nameplateBase, "OnOptionsUpdated", function(namePlate)
-            hooksecurefunc(namePlate.UnitFrame.BuffFrame, "UpdateBuffs", function(...)
-                self:OnUpdateBuffs(...)
-            end)
-        end)
-    end)
+    self:RegisterEvent("NAME_PLATE_UNIT_ADDED")
+    self:RegisterEvent("UNIT_AURA")
 end
 
-function NamePlatesComplete_AuraTooltipDriverMixin:OnUpdateBuffs(buffFrame, ...)
+function NamePlatesComplete_AuraTooltipDriverMixin:OnEvent(event, ...)
+    if event == "NAME_PLATE_UNIT_ADDED" then
+        self:OnNamePlateUnitAdded(...)
+    elseif event == "UNIT_AURA" then
+        self:OnUnitAura(...)
+    end
+end
+
+function NamePlatesComplete_AuraTooltipDriverMixin:OnNamePlateUnitAdded(unit)
+    self:OnUnitAura(unit, { isFullUpdate = true })
+end
+
+function NamePlatesComplete_AuraTooltipDriverMixin:OnUnitAura(unit, unitAuraInfo)
+    local namePlate = C_NamePlate.GetNamePlateForUnit(unit)
+    if namePlate then
+        self:UpdateBuffs(namePlate.UnitFrame.BuffFrame, namePlate.namePlateUnitToken, unitAuraInfo)
+    end
+end
+
+function NamePlatesComplete_AuraTooltipDriverMixin:UpdateBuffs(buffFrame, unit, unitAuraUpdateInfo)
     for _, buff in ipairs({ buffFrame:GetChildren() }) do
         if buff:IsMouseEnabled() then
             buff:EnableMouse(false)
